d51e9281bea5f5eb9725a8fa1777df01911c71dd
fayyazhm <fayyazhm@gmail.com>
2024-03-30 11:08:34 +0530

    second commit

--- src/test/test1.java
+++ src/test/test1.java
@@ -7,7 +7,9 @@ public class test1 {
 	@Test
 	public void Demo2() {
 		System.out.println("aaaaa");
-		
+		System.out.println("bbbbb");
+		System.out.println("ccccc");
+		System.out.println("dddddd");
 	}
 
 }
